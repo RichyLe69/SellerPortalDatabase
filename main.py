d8ae69acbcac64ac4e3c28301d08cf4f3dd7b201
richardle17 <richardle17@yahoo.com>
2021-05-08 19:32:47 -0700

    brief notes on requirements

---
+++ main.py
@@ -0,0 +1,23 @@
+
+
+
+# TODO
+# Requirements
+# invidiaul lists
+# individual consoles (to allow for multi processing)
+#
+
+
+
+
+# end to end process
+# create card list object from list
+# increment through list which contains the ID of the card
+# go to seller portal website
+# Login
+# parse the html table
+# ETL the data and append to a table and add it to output
+# sum up the stuff
+# use console to post summary of missing cards, missing prices, total prices
+# mysql table
+# data calculations, graph creations
